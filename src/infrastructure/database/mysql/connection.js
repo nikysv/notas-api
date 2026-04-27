b50cce478a53d62e7194b4db6a4c084b6ed5869b
nikysv <serratenikoll@gmail.com>
2026-04-26 21:09:30 -0400

    fixes

--- src/infrastructure/database/mysql/connection.js
+++ src/infrastructure/database/mysql/connection.js
@@ -1,22 +1,26 @@
-import { Sequelize } from "sequelize";
+import SequelizePkg from "sequelize";
+
+const Sequelize = SequelizePkg.Sequelize || SequelizePkg;
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
-    process.env.MYSQL_PASSWORD,{
-        host: process.env.MYSQL_HOST,
-        dialect: "mysql",
-    }); 
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+  },
+);
 
 export const connectMysql = async () => {
-    try {
-        await sequelize.authenticate(); 
-        await sequelize.sync({alter: true});
-        console.log('Connected to MySQL');
-    } catch (error) {
-        console.error('Error connecting to MySQL:', error);
-        process.exit(1);
-    }
-  };
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    console.log("Connected to MySQL");
+  } catch (error) {
+    console.error("Error connecting to MySQL:", error);
+    process.exit(1);
+  }
+};
 
 export default sequelize;
